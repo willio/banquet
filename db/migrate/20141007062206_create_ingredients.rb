f276fd5beb9bccf02ec7eaeda5af6515ad3390d7
Will Wiriawan <wiriawan@gmail.com>
2014-10-07 14:40:42 +0800

    fixing wills mistake

--- db/migrate/20141007062206_create_ingredients.rb
+++ db/migrate/20141007062206_create_ingredients.rb
@@ -1,6 +1,6 @@
 class CreateIngredients < ActiveRecord::Migration
   def change
-  	create_table :ingredient |t|
+  	create_table :ingredients do |t|
   		t.string :name
   		t.float :price
   		t.integer :item_id
